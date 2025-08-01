691ecdac35affe813905c1d4751538212a18120e
Cccabarcal <cccabarcal@eafit.edu.co>
2025-08-01 11:46:04 -0500

    busqueda de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='movies/images/')
+    url = models.URLField(max_length=200)
+
+    
