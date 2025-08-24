6b7f9b086c1f402e831bee3963cd3abbcceb6d77
Cristian <cccabarcal@eafit.edu.co>
2025-08-24 10:09:18 -0500

    models.py

--- movie/models.py
+++ movie/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    director = models.CharField(max_length=100)
+    director = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movies/images/')
-    url = models.URLField(max_length=200)
+    url = models.URLField(blank=True)
+    genre = models.CharField(max_length=250, blank=True)
+    year = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
     
